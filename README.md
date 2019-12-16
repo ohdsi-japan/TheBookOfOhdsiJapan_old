4d28cc26f04583750a165c0955ff857b6367e1b2
Masafumi Okada <mf@okadajp.org>
2019-12-17 00:54:02 +0900

    Added files from The OHDSI Book Korea files.
    To begin translation into Japanese, it was suggested to start with completed Korean translation.

---
+++ README.md
@@ -0,0 +1,12 @@
+# TheBookOfOhdsi
+
+오딧세이 한국 네트워크 책입니다.
+
+집필 중인 온라인 판은 하기 주소에서 보실 수 있습니다. 
+
+https://ohdsi-korea.github.io/TheBookOfOhdsiKorea/
+
+책 수정에 대한 설명은 [the OHDSI Wiki](http://www.ohdsi.org/web/wiki/doku.php?id=projects:workgroups:notes_on_writing_text_for_the_book) 와 [Book of OHDSI Cheat Sheet](https://raw.githubusercontent.com/OHDSI/TheBookOfOhdsi/master/extras/CheatSheet.pdf) 을 확인하시기 바랍니다. 
+[수정 방법 동영상](https://youtu.be/HAqLRwgEqZw) 을 참고하실 수도 있습니다. 
+
+Under the Development
---
+++ extras/PackageMaintenance.R
@@ -0,0 +1,121 @@
+# Remove orphan images ---------------------------------------------------------
+
+rmdFiles <- list.files(pattern = ".*Rmd")
+
+getImageRefs <- function(rmdFile) {
+  rmd <- readChar(rmdFile, file.info(rmdFile)$size)
+  matches <- gregexpr("images[^'\")]*\\.(png|PNG|jpg|JPG)", rmd)
+  imageRefs <- regmatches(rmd, matches)[[1]]
+  return(imageRefs)
+}
+imageRefs <- lapply(rmdFiles, getImageRefs)
+imageRefs <- do.call(c, imageRefs)
+imageRefs <- unique(imageRefs)
+imageRefs <- c(imageRefs, file.path("images", "important.png"), file.path("images", "summary.png"))
+
+images <- list.files("images", ".*\\.(png|PNG|jpg|JPG)", recursive = TRUE)
+images <- file.path("images", images)
+
+toDelete <- images[!(images %in% imageRefs)]
+toDelete
+
+unlink(toDelete)
+
+# Fetch current software versions -----------------------------------------------
+
+packages <- c("CohortMethod",
+              "SelfControlledCaseSeries",
+              "SelfControlledCohort",
+              "CaseControl",
+              "CaseCrossover",
+              "PatientLevelPrediction",
+              "EmpiricalCalibration",
+              "MethodEvaluation",
+              "EvidenceSynthesis",
+              "DatabaseConnector",
+              "SqlRender",
+              "Cyclops",
+              "ParallelLogger",
+              "FeatureExtraction")
+
+getPackageVersion <- function(package) {
+  url <- sprintf("https://raw.githubusercontent.com/OHDSI/%s/master/DESCRIPTION", package)
+  description <- httr::GET(url)
+  description <- httr::content(description)
+  matches <- gregexpr("Version:[^\n]*\n", description)
+  version <- regmatches(description, matches)[[1]]
+  version <- gsub("Version:[ \t]*", "", version)
+  version <- gsub("[ \t]*\r?\n", "", version)
+  return(version)
+}
+
+versions <- sapply(packages, getPackageVersion)
+packageVersions <- data.frame(package = packages, version = versions)
+write.csv(packageVersions, "PackageVersions.csv", row.names = FALSE)
+
+# Turn implicit subsubsections into explicit ones ----------------------------
+rmdFiles <- list.files(pattern = ".*Rmd")
+
+convertSubsubsection <- function(rmdFile) {
+  rmd <- readChar(rmdFile, file.info(rmdFile)$size)
+  matches <- gregexpr("\n\\*\\*[^*\n]*\\*\\*[ \t]*[\r]", rmd)
+  headers <- regmatches(rmd, matches)[[1]]
+  if (length(headers) > 0) {
+    for (header in headers) {
+      newHeader   <- gsub("\n\\*\\*", "\n#### ", gsub("\\*\\*[ \t]*[\r]", " {-}\r", header))
+      rmd <- gsub(header, newHeader, rmd, fixed = TRUE)
+    }
+    sink(rmdFile)
+    rmd <- gsub("\r", "", rmd)
+    cat(rmd)
+    sink()
+  }
+}
+lapply(rmdFiles, convertSubsubsection)
+
+# Fix header capitalization --------------------------------------------------
+ignoreWords <- c("in", "the", "a", "an", "of", "for", "and", "to", "into", "from", "xSpec", "xSens", "on", "this", "as")
+
+fixWordCapitalization <- function(word) {
+  if (word %in% ignoreWords) {
+    return(word)
+  } else {
+    return(paste0(toupper(substr(word, 1, 1)), substr(word, 2, nchar(word))))
+  }
+}
+
+fixSingleHeader <- function(header) {
+  words <- strsplit(header, " ")[[1]]
+  words <- sapply(words, fixWordCapitalization)
+  header <- paste(words, collapse = " ")
+  words <- strsplit(header, "-")[[1]]
+  if (length(words) > 1) {
+    words <- sapply(words, fixWordCapitalization)
+    header <- paste(words, collapse = "-")
+  }
+  return(header)
+}
+
+rmdFiles <- list.files(pattern = ".*Rmd")
+
+fixHeaderCapitalization <- function(rmdFile) {
+  print(rmdFile)
+  rmd <- readChar(rmdFile, file.info(rmdFile)$size)
+  matches <- gregexpr("\n(##|###)[^\r\n]*[\r]", rmd)
+  headers <- regmatches(rmd, matches)[[1]]
+  if (length(headers) > 0) {
+    for (header in headers) {
+      newHeader   <- fixSingleHeader(header)
+      rmd <- gsub(header, newHeader, rmd, fixed = TRUE)
+    }
+    sink(rmdFile)
+    rmd <- gsub("\r", "", rmd)
+    cat(rmd)
+    sink()
+  }
+}
+lapply(rmdFiles, fixHeaderCapitalization)
+
+
+
+
